cb3ae532d497ac86c28b4f61ee6f1540330b30b0
nreid <nreid@BOBICKLE>
2025-09-17 15:00:07 -0400

    Add C# Project & Add first line of Intro.

---
+++ reid-nathan-a1-derelictstar/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Cesletial Voyage Cyber Interface... Ver. 3.1. © 2189\nWelcome to the Celestial Voyage experience. You are currently subscribed" +
+    "to our premium plan for 7,400 credits a month.\nERROR 40176... Vision Module 9.5 boot-up failed!\nERROR 40198... Memory Restore failed to load!" +
+    "\nPlease input user information...");
